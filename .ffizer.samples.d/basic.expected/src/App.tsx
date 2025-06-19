09caad220315cd28312c2703121d0a6273cb3960
Viacheslav Shvets <slava@shvec.com>
2025-06-19 21:02:51 +0300

    fix: fix template

--- .ffizer.samples.d/basic.expected/src/App.tsx
+++ .ffizer.samples.d/basic.expected/src/App.tsx
@@ -1,7 +1,7 @@
 import { css } from "../styled-system/css";
 
 function App() {
-	return <div className={css({ fontSize: "4xl", fontWeight: "bold" })}></div>;
+	return <div className={css({ fontSize: "4xl", fontWeight: "bold" })}>Hello World</div>;
 }
 
 export default App;
