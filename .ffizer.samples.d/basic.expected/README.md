46d48ea4fa36134b84ed6e12286973f3c9ed2e47
Viacheslav Shvets <slava@shvec.com>
2025-06-19 20:20:33 +0300

    init ffizer starter

---
+++ .ffizer.samples.d/basic.expected/README.md
@@ -0,0 +1,5 @@
+# MyApp
+
+A React + PandaCSS app
+
+Generated with [ffizer](https://ffizer.github.io/).
---
+++ .ffizer.samples.d/basic.expected/src/App.tsx
@@ -0,0 +1,7 @@
+import { css } from "../styled-system/css";
+
+function App() {
+	return <div className={css({ fontSize: "4xl", fontWeight: "bold" })}></div>;
+}
+
+export default App;
