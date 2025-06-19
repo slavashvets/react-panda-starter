307960b1785d47e4a4d06decd80308b656f65064
Viacheslav Shvets <slava@shvec.com>
2025-06-19 21:16:52 +0300

    fix: fix format

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,11 @@
 import { css } from "../styled-system/css";
 
 function App() {
-	return <div className={css({ fontSize: "4xl", fontWeight: "bold" })}>Hello World</div>;
+	return (
+		<div className={css({ fontSize: "4xl", fontWeight: "bold" })}>
+			Hello World
+		</div>
+	);
 }
 
 export default App;
